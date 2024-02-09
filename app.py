725752f549939bc92077e5eacdb7a071895d85b7
Leonardo Racic <145390423+leonardo-racic@users.noreply.github.com>
2024-02-09 13:07:11 +0100

    Basic login/signup logic
    Fixed errors, now we have to see how to store login info in the server

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
 from flask import Flask, render_template, request
 
 
-app = Flask(__name__)
+app: Flask = Flask(__name__)
+
+
+def is_login_valid(username: str, password: str) -> bool:
+    if username == "" or password == "":
+        return False
+    return True
 
 
 @app.route("/")
-def main():
-    return render_template("website.html")
+def main() -> None:
+    return render_template("login.html")
 
 
-@app.route("/login", methods=["POST"])
-def login():
-    data = request.data
-    print(data)
+@app.route("/login", methods=["POST", "GET"])
+def login() -> str:
+    error: str = ""
+    print("/login was accessed")
+    if request.method == "POST":
+        print("/login POST entered")
+        input_username = request.form.get("username", "")
+        input_password = request.form.get("password", "")
+        if is_login_valid(input_username, input_password):
+            return "<p>Logged in.</p>"
+        else:
+            error = "The password and/or the username is invalid."
+            print(error)
+    else:
+        error = "I haven't coded the get method yet."
+
+    return error
