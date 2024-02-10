20a7cb3b6db7f557d83a29c81cc52ac7d9a938b6
gand-elf13 <145144448+gand-elf13@users.noreply.github.com>
2024-02-10 12:47:19 +0100

    started working
    i installed venv and Flask, it might break personal install... also created home page and did cleaner redirects

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 
+import time
 
 app: Flask = Flask(__name__)
 
@@ -12,22 +13,27 @@ def is_login_valid(username: str, password: str) -> bool:
 
 @app.route("/")
 def main() -> None:
-    return render_template("login.html")
+    return render_template("home_page.html")
 
 
 @app.route("/login", methods=["POST", "GET"])
 def login() -> str:
     error: str = ""
+    
     print("/login was accessed")
     if request.method == "POST":
         print("/login POST entered")
         input_username = request.form.get("username", "")
         input_password = request.form.get("password", "")
         if is_login_valid(input_username, input_password):
-            return "<p>Logged in.</p>"
+            
+            return redirect(url_for('main'))
         else:
             error = "The password and/or the username is invalid."
             print(error)
+    if request.method == "GET":
+        return render_template("login.html")
+    
     else:
         error = "I haven't coded the get method yet."
 
