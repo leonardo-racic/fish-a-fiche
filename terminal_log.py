9db756eaa93e2ba4b3fe2a25b4a8a3795e5c5abb
Dosha <145390423+leonardo-racic@users.noreply.github.com>
2024-02-24 09:52:04 +0100

    Logging system
    - I added a log file
    - I added functions that will be used to debug/log when the server is active
    - We just need to check what and where to log

---
+++ terminal_log.py
@@ -0,0 +1,51 @@
+import logging
+
+
+log_file = "debug.log"
+
+
+def run_logging() -> None:
+    clear_log_file()
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s   (%(name)s)   %(message)s",
+        datefmt="[%m/%d/%y %h:%m:%s]",
+        filename=log_file,
+    )
+    inform("Logging started")
+    debug("Logging debug message")
+    warn("Logging warning message")
+    log_error("Logging error message")
+
+
+def debug(msg: str, *args) -> None:
+    logging.debug(msg, *args)
+
+
+def inform(msg: str, *args) -> None:
+    logging.info(msg, *args)
+
+
+def warn(msg: str, *args) -> None:
+    logging.warning(msg, *args)
+
+
+def log_error(msg: str, *args) -> None:
+    logging.error(msg, *args)
+
+
+def log_exception(msg: str, *args) -> None:
+    logging.exception(msg, *args)
+
+
+def log_critical(msg: str, *args) -> None:
+    logging.critical(msg, *args)
+
+
+def clear_log_file() -> None:
+    with open(log_file, "w"):
+        pass
+
+
+if __name__ == "__main__":
+    run_logging()
