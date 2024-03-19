b40f0a3fb2e9ecec18b9942324a5427aaebdd5b1
Leonardo Racic <145390423+leonardo-racic@users.noreply.github.com>
2024-03-19 09:30:23 +0100

    Primitive form of a comment section
    I did a first version, we'll probably upgrade it later and stylise it

--- modules/handle_cheat_sheet.py
+++ modules/handle_cheat_sheet.py
@@ -3,28 +3,11 @@ from json import load
 from .server_account_manager import ServerAccountManager, get_hash
 from .cheat_sheet_manager import CheatSheetManager
 from .cheat_sheet_module import CheatSheet
+from .account_module import Account
 
 
 def get_form_data() -> dict:
     return dict(request.form)
-
-
-def handle_test(server_account_manager: ServerAccountManager) -> Response:
-    with open("modules/cheat_sheet_test.json") as f:
-        data = load(f)
-    author_username: str = server_account_manager.get_current_username_from_token(data["author_token"])
-    return render_template(
-        "cheat_sheet.html",
-        title=data["title"],
-        author_username=author_username,
-        is_logged_in=server_account_manager.is_user_logged_in(),
-        context=data["context"],
-        content=data["content"],
-        date=data["date"],
-        likes=data["likes"],
-        dislikes=data["dislikes"],
-        comments=data["comments"],
-    )
 
 
 def handle_cheat_sheet(
@@ -39,6 +22,11 @@ def handle_cheat_sheet(
         if cheat_sheet_info == {} or author_username == "":
             return Response("Well uhhhhh", status=404)
 
+
+        comments: list = cheat_sheet_info["comments"]
+        for i in range(len(comments)):
+            comments[i]["username"] = server_account_manager.get_account_info_by_token(comments[i]["id"])["username"]
+        
 
         return render_template(
             "cheat_sheet.html",
@@ -59,6 +47,14 @@ def handle_cheat_sheet(
     
 
     elif request.method == "POST":
+        form_data: dict = get_form_data()
+        content: str = form_data.get("comment_input")
+        user_account_id: str = server_account_manager.get_user_account_token()
+        new_comment: dict = {
+            "content": content,
+            "id": user_account_id
+        }
+        cheat_sheet_manager.add_comment_to_cheat_sheet(token, new_comment)
         return redirect(f"/cheat-sheet/{token}")
 
 
