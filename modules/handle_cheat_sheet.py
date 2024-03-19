771d3d94837a6cb54ea8809ab277e2308c05c203
Leonardo Racic <145390423+leonardo-racic@users.noreply.github.com>
2024-03-19 08:50:16 +0100

    Comments starting point
    - I made the logi nand sign-up pretty
    - I started working on comments

--- modules/handle_cheat_sheet.py
+++ modules/handle_cheat_sheet.py
@@ -5,6 +5,10 @@ from .cheat_sheet_manager import CheatSheetManager
 from .cheat_sheet_module import CheatSheet
 
 
+def get_form_data() -> dict:
+    return dict(request.form)
+
+
 def handle_test(server_account_manager: ServerAccountManager) -> Response:
     with open("modules/cheat_sheet_test.json") as f:
         data = load(f)
@@ -28,36 +32,44 @@ def handle_cheat_sheet(
     server_account_manager: ServerAccountManager,
     token: str
 ) -> Response:
-    cheat_sheet_info: dict = cheat_sheet_manager.get_cheat_sheet_info(token)
-    author_token: str = cheat_sheet_info["author_token"]
-    author_username: str = server_account_manager.get_current_username_from_token(author_token)
-    if cheat_sheet_info == {} or author_username == "":
-        return Response("Well uhhhhh", status=404)
+    if request.method == "GET":
+        cheat_sheet_info: dict = cheat_sheet_manager.get_cheat_sheet_info(token)
+        author_token: str = cheat_sheet_info["author_token"]
+        author_username: str = server_account_manager.get_current_username_from_token(author_token)
+        if cheat_sheet_info == {} or author_username == "":
+            return Response("Well uhhhhh", status=404)
 
 
-    return render_template(
-        "cheat_sheet.html",
-        title=cheat_sheet_info["title"],
-        author_username=author_username,
-        author_hashed_token=get_hash(author_token),
-        token=get_hash(server_account_manager.get_user_account_token()),
-        cheat_sheet_token=token,
-        is_user_author=server_account_manager.get_user_account_token() == cheat_sheet_info["author_token"],
-        logged_in=server_account_manager.is_user_logged_in(),
-        context=cheat_sheet_info["context"],
-        content=cheat_sheet_info["content"],
-        date=cheat_sheet_info["date"],
-        likes=cheat_sheet_info["likes"],
-        dislikes=cheat_sheet_info["dislikes"],
-        comments=cheat_sheet_info["comments"],
-    )
+        return render_template(
+            "cheat_sheet.html",
+            title=cheat_sheet_info["title"],
+            author_username=author_username,
+            author_hashed_token=get_hash(author_token),
+            token=get_hash(server_account_manager.get_user_account_token()),
+            cheat_sheet_token=token,
+            is_user_author=server_account_manager.get_user_account_token() == cheat_sheet_info["author_token"],
+            logged_in=server_account_manager.is_user_logged_in(),
+            context=cheat_sheet_info["context"],
+            content=cheat_sheet_info["content"],
+            date=cheat_sheet_info["date"],
+            likes=cheat_sheet_info["likes"],
+            dislikes=cheat_sheet_info["dislikes"],
+            comments=cheat_sheet_info["comments"],
+        )
+    
+
+    elif request.method == "POST":
+        return redirect(f"/cheat-sheet/{token}")
+
+
+    return "not done yet"
 
 
 def handle_create_cheat_sheet(
     cheat_sheet_manager: CheatSheetManager,
     server_account_manager: ServerAccountManager,
 ) -> Response:
-    cheat_sheet_data: dict = dict(request.form)
+    cheat_sheet_data: dict = get_form_data()
     if server_account_manager.is_user_logged_in():
         cheat_sheet_data["author_token"] = server_account_manager.get_user_account_token()
         cheat_sheet: CheatSheet = cheat_sheet_manager.create_new_cheat_sheet(cheat_sheet_data)
@@ -76,7 +88,7 @@ def handle_modify_cheat_sheet(
     if cheat_sheet is None:
         return Response(f"CheatSheet({token}) does not exist.", status=404)
     if request.method == "POST":
-        new_cheat_sheet_info: dict = dict(request.form)
+        new_cheat_sheet_info: dict = get_form_data()
         cheat_sheet_manager.modify_cheat_sheet(token, new_cheat_sheet_info)
         return redirect(url_for("main"))
     elif request.method == "GET":
