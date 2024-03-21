71c835ba3d5c6a6ce29f7115a7b9b7920e7257fe
gand-elf13 <duclosma@icloud.com>
2024-03-21 11:34:00 +0100

    documented handle upload

--- modules/handle_upload.py
+++ modules/handle_upload.py
@@ -1,3 +1,33 @@
+"""
+This module contains the code for the cheatsheet uploader.
+
+The main classes are:
+- CheatSheet: represents a single cheatsheet
+- CheatSheetManager: manages a list of cheatsheets
+- ServerAccountManager: manages the user's authentication with the server
+
+The main functions are:
+- handle_upload: handles the upload of a new cheatsheet
+- create_cheat_sheet: creates a new CheatSheet from the uploaded file
+- read_md: reads the contents of a markdown file
+
+The main routes are:
+- /upload: handles the upload form
+
+The main templates are:
+- upload.html: the upload form
+
+The main static files are part of the bootstrap library.
+
+The main dependencies are:
+- flask: the web framework used
+- werkzeug: utilities for working with Flask
+- markdown: for parsing markdown files
+- bootstrap
+
+Note: this docstring is a modified version of the Google Python Style Guide.
+"""
+
 import os
 from flask import Flask, flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
@@ -19,11 +49,23 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 csm = CheatSheetManager()
 
 def allowed_file(filename):
+    """
+    This function checks if a file with the given name has a valid extension.
+
+    :param filename: the name of the file to check
+    :return: True if the file has a valid extension, False otherwise
+    """
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def handle_upload(server_account_manager: ServerAccountManager):
+    """
+    This function handles the upload of a new cheatsheet.
 
+    :param server_account_manager: the server account manager used to authenticate the user
+    :return: redirect to the same page if upload unsuccesfull, or a page indicating that the
+             upload was succesfull otherwise.
+    """
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -52,13 +94,16 @@ def handle_upload(server_account_manager: ServerAccountManager):
     print(logged_in)
     return render_template('upload.html',logged_in=logged_in)
 
-
 def create_cheat_sheet(server_account_manager: ServerAccountManager):
+    """
+    This function creates a new CheatSheet object from the uploaded file.
+
+    :param server_account_manager: the server account manager used to authenticate the user
+    :return: the new CheatSheet object
+    """
     file = request.files['file']
     title = request.form.get("title")
-    print("create cs starting")
     author_token = server_account_manager.get_user_account_token()
-    print("create cs ok")
     content = read_md(UPLOAD_FOLDER+"/"+str(secure_filename(request.form.get("title"))))
     description = request.form.get("description")
     keywords = request.form.get("keywords")
@@ -69,5 +114,11 @@ def create_cheat_sheet(server_account_manager: ServerAccountManager):
     return new_cs
 
 def read_md(file):
+    """
+    This function reads the contents of a markdown file.
+
+    :param file: the path to the markdown file
+    :return: the contents of the markdown file
+    """
     with open(file) as md:
         return md.read()
