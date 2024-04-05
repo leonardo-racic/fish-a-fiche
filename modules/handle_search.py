85da1fcd8ba4033ca886df89c7b0bc680df92efd
Leonardo Racic <145390423+leonardo-racic@users.noreply.github.com>
2024-04-05 13:03:05 +0200

    Search update
    - profiles can be searched
    - however, there is a bug...

--- modules/handle_search.py
+++ modules/handle_search.py
@@ -1,12 +1,21 @@
 import json
 from singletons import render_html
 from flask import Response, request, redirect
-from .server_account_manager import ServerAccountManager
-from environment_variable import cs_path
+from .server_account_manager import ServerAccountManager, get_hash
+from environment_variable import cs_path, account_path
+
+
+def get_profiles() -> list:
+    with open(account_path) as f:
+        data: dict = json.loads(f.read())["accounts"]
+    for id, info in list(data.items()):
+        data[id]["id"] = get_hash(info["id"])
+    data_values: list = list(data.values())
+    return data_values
 
 
 def filter_title(name: str) -> list:
-    def filter_func(current_dict) -> list:
+    def filter_func(current_dict: dict) -> bool:
         current_title: str = current_dict["title"]
         condition: bool = name.casefold() in current_title
         return condition
@@ -20,8 +29,21 @@ def filter_title(name: str) -> list:
     if name != "":
         result: list = list(filter(filter_func, data_values))
         return result
-    else:
-        return data_values
+    return data_values
+
+
+def filter_profiles(username: str) -> list:
+    def filter_func(current_dict: dict) -> bool:
+        current_username: str = current_dict["username"]
+        condition: bool = username.casefold() in current_username
+        return condition
+
+
+    data_values: list = get_profiles()
+    if username != "":
+        result: list = list(filter(filter_func, data_values))
+        return result
+    return data_values
 
 
 def sort_results(cs: list, by: str) -> list:
@@ -33,10 +55,12 @@ def handle_search(sam: ServerAccountManager, name: str) -> Response:
     if request.method == 'POST':
         return redirect(f'/search/{request.form.get("title")}')
     else:
+        profiles: list = filter_profiles(name)
         return render_html(
             'search.html',
             sam,
             search_title=name,
+            profiles=profiles,
             cheat_sheet=sort_results(filter_title(name),'likes'),
         )
 
@@ -48,6 +72,7 @@ def handle_search_empty(sam: ServerAccountManager) -> Response:
         return render_html(
             'search.html',
             sam,
-            cheat_sheet=sort_results(filter_title(''),'likes'),
+            cheat_sheet=sort_results(filter_title(''), 'likes'),
+            profiles=get_profiles(),
             search_title="",
         )
