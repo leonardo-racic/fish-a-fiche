82222af59c59c84d223b5be151b415395e7c4774
gand-elf13 <145144448+gand-elf13@users.noreply.github.com>
2024-05-15 14:34:38 +0200

    updated environment variable to the site version,uncomment to your needs

--- environment_variable.py
+++ environment_variable.py
@@ -1,8 +1,14 @@
 import os
 
 
-cs_path: str = 'cheat_sheet.json'
-account_path: str = 'accounts.json'
-upload_path: str = os.path.join("static", "upload")
-pfp_path: str = os.path.join("static", "pfp")
-reports_path: str = 'reports.json'
+#cs_path: str = 'cheat_sheet.json'
+#account_path: str = 'accounts.json'
+#upload_path: str = os.path.join("static", "upload")
+#pfp_path: str = os.path.join("static", "pfp")
+#reports_path: str = 'reports.json'
+
+cs_path: str = "/home/greandelephant/leonardo-racic.github.io/cheat_sheet.json"
+account_path: str = "/home/greandelephant/leonardo-racic.github.io/accounts.json"
+log_path: str = "/home/greandelephant/leonardo-racic.github.io/debug.log"
+upload_path: str = "/home/greandelephant/leonardo-racic.github.io/sheets"
+reports_path: str = "/home/greandelephant/leonardo-racic.github.io/reports.json"
